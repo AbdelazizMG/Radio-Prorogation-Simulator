37fbba624ece8ac5e1ac20f6afd532606f9f8314
Mohammad Wael <100419696+MoeWael@users.noreply.github.com>
2023-04-04 02:00:54 +0200

    Fixed Login

--- Test.m
+++ Test.m
@@ -1,1 +1,15 @@
-pw = passwordUI('Create', 'AlphaNum')
+file = jsondecode(fileread("database.json"));
+usernames = fieldnames(file).';
+
+passwords = {}
+
+for i = 1:length(usernames)
+    
+   passwords{i} = getfield(file,usernames{i});
+end
+
+for i = 1:length(usernames)
+    
+   struct1.(usernames{i}) = passwords(i);
+end
+
