e92ee2e93c221f08139daf7d4b82c906936d8445
Muhammed Abdallah Mahmoud <maxmohamed30@gmail.com>
2023-04-07 01:00:44 +0200

    6/4 final edits
    final touches

--- estimation_f.m
+++ estimation_f.m
@@ -1,6 +1,8 @@
-function  [n_estimated_value,sigma_estimated_value,PL_d_decimal] = estimation_f(sigma,d,PL_do_decimal,n,do,number_of_samples_per_distance)
+function  [n_estimated_value,sigma_estimated_value,PL_d_decimal,model] = estimation_f(sigma,d,PL_do_decimal,n,do,number_of_samples_per_distance)
+
 syms n_estimated;
-x_sigma = sigma .* randn(50,1);
+PL_d_decimal=[];
+x_sigma = sigma .* randn(number_of_samples_per_distance,1);
 sum2=0;
 for d_counter = 1 : 1 : length(d)   
     Mean = PL_do_decimal + 10 * n * log10(d(d_counter)/do);
@@ -8,12 +10,15 @@ for d_counter = 1 : 1 : length(d)
     PL_d_dB(:,d_counter) = 10*log10(PL_d_decimal(:,d_counter));
     PL_d_dBm(:,d_counter) = PL_d_dB(:,d_counter) + 30;
     PL_Model(:,d_counter) = PL_do_decimal + 10 * n_estimated * log10(d(d_counter)/do);
-    sum2 =sum2+   (PL_d_decimal(:,d_counter) - PL_Model(d_counter)) .^ 2;
+    sum2 =sum2 +   (PL_d_decimal(:,d_counter) - PL_Model(d_counter)) .^ 2;
 end
 
 exp = sum(sum2);
 tt =  diff(exp/(number_of_samples_per_distance*length(d)));
 n_estimated_value = double(solve(tt==0,n_estimated));
-sigma_estimated_value = sqrt(double(subs(exp/(length(d)*number_of_samples_per_distance),n_estimated,n_estimated_value)));
 
+sigma_estimated_value = sqrt(double(subs(exp/(length(d)*number_of_samples_per_distance),n_estimated,n_estimated_value)));
+for d_counter = 1 : 1 : length(d)
+      model(d_counter) = PL_do_decimal + 10 *  n_estimated_value * log10(d(d_counter)/do);  
+end
 end
