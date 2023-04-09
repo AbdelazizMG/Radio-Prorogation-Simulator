ff5ce1017143d9a5b0b71fe4f2d0092b70268f49
Muhammed Abdallah Mahmoud <maxmohamed30@gmail.com>
2023-04-10 00:23:39 +0200

    final

--- estimation_file.m
+++ estimation_file.m
@@ -1,4 +1,4 @@
-function  [n_estimated_value,sigma_estimated_value,avg_array,model] = estimation_file(PL_d,d,PL_do_decimal,do)
+function  [n_estimated_value,sigma_estimated_value,avg_array,model] = estimation_file(PL_d,d,PL_do,do)
 
 syms n_estimated;
 sum2=0;
@@ -7,7 +7,7 @@ mean_array= PL_d;
 x_sigma = 2 .* randn(20,1);
 for d_counter = 1 : 1 : length(d)  
     avg_array(:,d_counter) = mean_array(d_counter) + x_sigma;
-    PL_Model(:,d_counter) = PL_do_decimal + 10 * n_estimated * log10(d(d_counter)/do);
+    PL_Model(:,d_counter) = PL_do + 10 * n_estimated * log10(d(d_counter)/do);
     sum2 =sum2 +   (avg_array(:,d_counter) - PL_Model(d_counter)) .^ 2;
 end
 number_of_samples_per_distance = length(avg_array(:,1));
@@ -17,6 +17,6 @@ n_estimated_value = double(solve(tt==0,n_estimated));
 
 sigma_estimated_value = sqrt(double(subs(exp/(length(d)*number_of_samples_per_distance),n_estimated,n_estimated_value)));
 for d_counter = 1 : 1 : length(d)
-      model(d_counter) = PL_do_decimal + 10 *  n_estimated_value * log10(d(d_counter)/do);  
+      model(d_counter) = PL_do + 10 *  n_estimated_value * log10(d(d_counter)/do);  
 end
 end
